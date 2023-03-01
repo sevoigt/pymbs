43dcf5b294d94ff715fd1cc9fc3c9ba8efd6a17b
svoigt <svoigt@gmx.de>
2023-03-01 19:04:57 +0100

    C# export works for trivial model works

--- test/csharp/project/Main.cs
+++ test/csharp/project/Main.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World or what?!");
+using MathNet.Numerics.LinearAlgebra;
+
+int main()
+{
+    Matrix<double> a = CreateMatrix.Dense<double>(2, 2, 5);
+
+    //Console.WriteLine(a[0,0]);
+    //Console.WriteLine((2*a)[1,0]);
+
+    pendulum model = new pendulum();
+    
+    double[] y0 = new double[2]{1, 0};
+    double[] yout;
+
+    var res = model.pendulum_der_state(0, y0, out yout);
+
+    Console.WriteLine(yout[0].ToString());
+    Console.WriteLine(yout[1].ToString());
+
+    return 0;
+
+}
+
+main();
