462a5b5b35ee43426a034ed529c50060ea652f99
sevoigt <svoigt@gmx.de>
2024-03-11 22:12:27 +0100

    removed obsolete test script, fixed auxmath module

--- pymbs/input/auxmath.py
+++ pymbs/input/auxmath.py
@@ -1,39 +1,17 @@
 # -*- coding: utf-8 -*-
-'''
-This file is part of PyMbs.
-
-PyMbs is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as 
-published by the Free Software Foundation, either version 3 of
-the License, or (at your option) any later version.
-
-PyMbs is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public 
-License along with PyMbs.
-If not, see <http://www.gnu.org/licenses/>.
-
-Copyright 2011, 2012 Carsten Knoll, Christian Schubert, 
-                     Jens Frenkel, Sebastian Voigt
-'''
-
-'''
+"""
 Created on 05.07.2009
 
 @author: knoll
 
 
 Module containing auxillary mathematical classes.
+"""
 
-'''
 
-from PyMbs.Symbolics import Symbol, sympify, Basic
+from pymbs.symbolics import Symbol, sympify, Basic
 
-from PyMbs.Common.MbsElement import MbsElement
-from PyMbs.Common.Expression import Expression
+from pymbs.common.mbselement import MbsElement
 
 
 # !! unify with Expr.
